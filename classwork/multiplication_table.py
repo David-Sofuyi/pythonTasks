243e65926b27bf1ac7c0d047bc46b897e9b22243
David-Sofuyi <dayodavid54@gmail.com>
2026-04-20 10:37:49 +0100

    pushing my assignment

---
+++ classwork/multiplication_table.py
@@ -0,0 +1,18 @@
+# collect input from user
+# multiply the number to give
+#
+#
+#
+#
+
+user_number =int(input ("what is the number you wish to multiply: "))
+for number in range (11):
+    product = number * user_number
+    print ( number, "x" , user_number, " = ", product)
+    number+1
+    
+    
+    
+    
+    
+
